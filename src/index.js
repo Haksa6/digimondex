a29888d04add6bb70ec61a6b56d9004de4cdb373
Haksa6 <77583826+Haksa6@users.noreply.github.com>
2022-06-13 09:49:44 +0300

    fetch data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import './sass/main.scss';
 
-
-
-
-const boxes = document.querySelectorAll(".box");
-const img = document.querySelector(".box-image");
+const main = document.querySelector(".main");
 
 
 const getDigimons = async () => {
+
+  // Fetch the data
   const response = await fetch('https://digimon-api.vercel.app/api/digimon', {mode: 'cors'});
   const data = await response.json();
-  img.src = data[208].img;
-  console.log(data[0].img)
+
+  //Clear out the main screen before getting data
+  main.innerHTML = "";
+  for(let digimon in data){
+    const box = document.createElement("div");
+    box.classList.add("box");
+
+    const img = document.createElement("img");
+    img.classList.add("box-image");
+    img.src = data[digimon].img;
+
+    const text = document.createElement("p");
+    text.classList.add("box-text");
+    text.textContent = data[digimon].name;
+
+    const level = document.createElement("p");
+    level.classList.add("box-level");
+    level.textContent = data[digimon].level;
+
+    box.appendChild(img);
+    box.appendChild(text);
+    box.appendChild(level);
+
+    main.appendChild(box);
+  }
+  console.log(data.length)
 }
 
 getDigimons();
